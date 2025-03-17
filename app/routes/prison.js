524468d0bb5c00096a7310f3a66b60853f5f7819
MattHall-DWP <matt.hall1@engineering.digital.dwp.gov.uk>
2025-03-17 19:45:01 +0000

    Prison fix

--- app/routes/prison.js
+++ app/routes/prison.js
@@ -113,4 +113,63 @@ router.post('/prison/iteration-2/restart-sp', function (req, res) {
   }
 })
 
+// ITERATION three
+
+// Reason for stopping SP
+router.post('/prison/iteration-3/stop-sp', function (req, res) {
+  if (req.body['stop-sp'] === 'suspend') {
+    res.redirect('personal-suspended')
+  } else if (req.body['stop-sp'] === 'prison') {
+    res.redirect('date-entered-prison')
+  } else if (req.body['stop-sp'] === 'custody') {
+    res.redirect('personal-suspended')
+  } else {
+    res.redirect('personal')
+  }
+})
+
+router.post('/prison/iteration-3/date-entered-prison', (req, res) => {
+  res.redirect('in-prison')
+})
+
+// Is the claimant still in prison?
+router.post('/prison/iteration-3/in-prison', function (req, res) {
+  if (req.body['in-prison'] === 'yes') {
+    res.redirect('prison-overpayment')
+  } else {
+    res.redirect('date-left-prison')
+  }
+})
+
+router.post('/prison/iteration-3/date-left-prison', (req, res) => {
+  res.redirect('personal-prison')
+})
+
+// Prison overpayment referral
+router.post('/prison/iteration-3/prison-overpayment-1', function (req, res) {
+  if (req.body['prison-overpayment'] === 'yes') {
+    res.redirect('prison-debt-referral')
+  } else {
+    res.redirect('personal-prison-overpayment')
+  }
+})
+
+router.post('/prison/iteration-3/prison/iteration-1-debt-referral', (req, res) => {
+  res.redirect('personal-prison')
+})
+
+router.post('/prison/iteration-3/prison/iteration-1-restart-sp', (req, res) => {
+  res.redirect('personal')
+})
+
+// Reason for restarting SP
+router.post('/prison/iteration-3/restart-sp', function (req, res) {
+  if (req.body['restart-sp'] === 'prison') {
+    res.redirect('personal-left-prison')
+  } else if (req.body['restart-sp'] === 'suspend') {
+    res.redirect('personal-suspendedrestarted')
+  } else {
+    res.redirect('personal')
+  }
+})
 module.exports = router
